c8ee98e2bb38bb54658b9d4d767ee67996b9874b
Anuj Kumar Sony <anujkumarrr0@gmail.com>
2024-09-01 12:11:41 +0530

    massage update

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -11,6 +11,7 @@ const Contact = () => {
     <>
       <div className="container contact" id="contact">
         <h1>CONTACT ME</h1>
+        
         <div className="contact-icon"
         data-aos = "zoom-in"
         data-aos-duration = "1000">
@@ -32,8 +33,33 @@ const Contact = () => {
           <a href="mailto:anujkumarrr0@gmail.com" target="_blank" className="items">
             <SiGmail className="icons"/>
           </a>
+          
         </div>
+        <div className="contact-form">
+          <h2>Send a Message</h2>
+          <form action="https://api.web3forms.com/submit" method="POST">
+          <input type="hidden" name="access_key" value="e508f8c6-f7c1-42a4-a965-eb9ad5b2255d" />
+            <div className="form-group">
+              
+              <label htmlFor="name">Name:</label>
+              <input type="text" id="name" name="name" required />
+            </div>
+            <div className="form-group">
+              <label htmlFor="email">Email:</label>
+              <input type="email" id="email" name="email" required />
+            </div>
+            <div className="form-group">
+              <label htmlFor="message">Message:</label>
+              <textarea id="message" name="message" rows="5" required></textarea>
+            </div>
+            <button type="submit" className="submit-btn">Send Message</button>
+          </form>
+        </div>
+
+        
       </div>
+      
+      
     </>
   );
 };
